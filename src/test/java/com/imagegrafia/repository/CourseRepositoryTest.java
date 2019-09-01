3f48c91b533248b5d4888255bacd743cb0f80821
Ashish Yadhuvanshi <ashishkvs@gmail.com>
2019-09-01 15:41:37 +0530

    h2-console added junit test case

---
+++ src/test/java/com/imagegrafia/repository/CourseRepositoryTest.java
@@ -0,0 +1,34 @@
+package com.imagegrafia.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.imagegrafia.HibernateJpaMasteringApplication;
+import com.imagegrafia.entity.Course;
+
+//load spring entire context
+@RunWith(SpringRunner.class)
+//explict context
+@SpringBootTest(classes = HibernateJpaMasteringApplication.class)
+class CourseRepositoryTest {
+
+	@Autowired
+	CourseRepository courseRepository;
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testFindById() {
+		Course course = courseRepository.findById(1000L);
+		assertEquals("Java-OOPs", course.getName());
+	}
+
+}
