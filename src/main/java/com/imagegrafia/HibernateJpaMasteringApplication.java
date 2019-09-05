5953a2a1bfd4c73cf2d2330feb120a64582aad89
Ashish Yadhuvanshi <ashishkvs@gmail.com>
2019-09-06 00:42:38 +0530

    enity in depth

--- src/main/java/com/imagegrafia/HibernateJpaMasteringApplication.java
+++ src/main/java/com/imagegrafia/HibernateJpaMasteringApplication.java
@@ -24,19 +24,20 @@ public class HibernateJpaMasteringApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Course findById = courseRepository.findById(1000l);
-		logger.info("Course : {}", findById);
+//		Course findById = courseRepository.findById(1000l);
+//		logger.info("Course : {}", findById);
 		// we will face transaction Issue if @Transactional not used in repository
 		/*
 		 * No EntityManager with actual transaction available for current thread -
-		 * cannot reliably process 'remove' call; nested exception is
+		 * cannot reli
+		 * ably process 'remove' call; nested exception is
 		 * javax.persistence.TransactionRequiredException: No EntityManager with actual
 		 * transaction available for current thread - cannot reliably process 'remove'
 		 * call
 		 */
-		courseRepository.deleteById(1001L);
-		//insert data
-		courseRepository.save(new Course("Python"));
+//		courseRepository.deleteById(1001L);
+//		//insert data
+//		courseRepository.save(new Course("Python"));
 		//entity manager Transactional side effect
 		courseRepository.playWithEntityManager();
 	}
--- src/main/java/com/imagegrafia/repository/CourseRepository.java
+++ src/main/java/com/imagegrafia/repository/CourseRepository.java
@@ -40,15 +40,40 @@ public class CourseRepository {
 		return course;
 	}
 
-	public void playWithEntityManager() {
-		Course course = new Course("WEb Development");
-		entityManager.persist(course);
-		// entity manager keeps track of changes when it is under @Transactional
-		course.setName("Web Devlopment updated");
-		// even if we call persist before update
-		// so above course will be updated No matter which line we called persit()
-		// entire method will be under @Transactional
-		// entityManager.merge(course); //NOT REQUIRED to update
+	// LEVEL-1
+	/*
+	 * public void playWithEntityManager() { Course course = new
+	 * Course("WEb Development"); entityManager.persist(course); // entity manager
+	 * keeps track of changes when it is under @Transactional
+	 * course.setName("Web Devlopment updated"); // even if we call persist before
+	 * update // so above course will be updated No matter which line we called
+	 * persit() // entire method will be under @Transactional //
+	 * entityManager.merge(course); //NOT REQUIRED to update
+	 * 
+	 * }
+	 */
 
+	// LEVEL -2
+	public void playWithEntityManager() {
+		Course course1 = new Course("Angular js");
+		//call next value for hibernate_sequence
+		entityManager.persist(course1);
+
+		Course course2 = new Course("React js");
+		//call next value for hibernate_sequence
+		entityManager.persist(course2);
+
+		//perform insert operation for above two rows
+		entityManager.flush();
+
+		course1.setName("Angular js updated"); //this will not update
+		course2.setName("React js updated"); // BUT this will updated
+
+		//select query is fired on course1 obj (id=1,name="")
+		//override changes
+		entityManager.refresh(course1); //becz of refres above course didnt get updated
+
+		//this wont save updated value
+		entityManager.flush();
 	}
 }
